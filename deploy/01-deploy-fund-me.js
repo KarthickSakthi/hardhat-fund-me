5190db7cfcc97469b5c6258c2fb7ade395bfd3d6
Karthick S <karthicksakthi124@gmail.com>
2023-10-22 10:58:49 +0000

    hardhat helper congig changes init

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,4 +1,5 @@
 const { network } = require("hardhat");
+const {networkConfig} = require("../helper-hardhat-config");
 
 async function deployFun({getNamedAccount, deployments}){
     const {deploy, log}  = deployments;
@@ -6,6 +7,7 @@ async function deployFun({getNamedAccount, deployments}){
     const address="0x694AA1769357215DE4FAC081bf1f309aDC325306";
     const chainId= network.config.chainId;
 
+    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
     const fundMe = deploy("FundMe",{
         from: deployer,
         args:[address],
---
+++ helper-hardhat-config.js
@@ -0,0 +1,12 @@
+const networkConfig ={
+    5:{
+        name:"Goerli",
+        ethUsdPriceFeed:"0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e"
+    },
+    58008:{
+        name:"Sepolia",
+        ethUsdPriceFeed:"0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"
+    }
+}
+
+module.exports ={networkConfig}
